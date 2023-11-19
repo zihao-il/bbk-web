41bb7d6990cec663fbfe4d3b7909ef73beeb5755
zihao-il <41977021+zihao-il@users.noreply.github.com>
2023-11-19 20:03:18 +0800

    更新库&优化导入

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import {createApp} from 'vue'
 import App from './App.vue'
-import '@varlet/ui/es/style'
+// import '@varlet/ui/es/style'
+import '@varlet/ui/es/action-sheet/style/index'
 import './style.css'
 
 createApp(App).mount('#zihao_il')
