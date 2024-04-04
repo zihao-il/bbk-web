045b1eec30f659aa309f2b8ddb91598c8eeddf4b
zihao-il <41977021+zihao-il@users.noreply.github.com>
2024-04-04 17:43:26 +0800

    添加sitemap,pwa 更新库

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,20 @@ import components from 'unplugin-vue-components/vite'
 import autoImport from 'unplugin-auto-import/vite'
 import {VarletUIResolver} from 'unplugin-vue-components/resolvers'
 import viteCompression from "vite-plugin-compression2";
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(),
         viteCompression(),
+        Sitemap({
+            hostname: 'https://127.0.0.1/',  //你的域名
+            robots: [{
+                userAgent: '*',
+                allow: '/',
+                crawlDelay: 300
+            }],
+        }),
         components({
             resolvers: [VarletUIResolver()]
         }), autoImport({
